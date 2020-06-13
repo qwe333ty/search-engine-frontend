844fd776d16d509a954cc03b609322e04c08ad06
Aliaksandr Rahavoi <aliaksandr.rahavoi@gmail.com>
2020-06-13 11:18:26 +0300

    adding modules

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { ArticleDetailsComponent } from './components/article-details/article-details.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ArticlesComponent, ArticleComponent, ArticleListComponent, ArticleDetailsComponent],
   imports: [
     CommonModule
   ]
